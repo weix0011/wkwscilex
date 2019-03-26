21b790743d4272d36c2d600674a132cc6e22ba74
weix0011 <weix0011@e.ntu.edu.sg>
2019-03-26 17:02:35 +0800

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,39 @@
+#' Lecixon without Part of Speech
+#'
+#' Lexicon without part of speech is developed by Prof. Chris Khoo of
+#' Wee Kim Wee School of Information and Communication, Nanyang
+#' Technological University.
+#'
+#' @format A data frame with two variables: \code{term} and \code{sentiment}.
+'without_pos'
+
+#' Lecixon with Part of Speech
+#'
+#' Lexicon with part of speech is developed by Prof. Chris Khoo of
+#' Wee Kim Wee School of Information and Communication, Nanyang
+#' Technological University.
+#'
+#' @format A data frame with three variables: \code{term}, \code{POS}
+#' and \code{sentiment}.
+'with_pos'
+
+#' Multiword terms
+#'
+#' Lexicon that contains multiword terms, a.k.a short phrases.
+#' It is developed by Prof. Chris Khoo of
+#' Wee Kim Wee School of Information and Communication, Nanyang
+#' Technological University.
+#'
+#' @format A data frame with two variables: \code{term} and \code{sentiment}.
+'multiword_terms'
+
+#' Modifier lecixon
+#'
+#' Lexicon contains modifiers such as absolutely and barely.
+#' It is developed by Prof. Chris Khoo of
+#' Wee Kim Wee School of Information and Communication, Nanyang
+#' Technological University.
+#'
+#' @format A data frame with three variables: \code{modifier}, \code{type}
+#' and \code{POS}.
+'modifier'
