cc953c603b8603b4c4d28ca06143cce3be0520f9
weix0011 <weix0011@e.ntu.edu.sg>
2019-04-17 16:20:14 +0800

    Updated package documentation and meta data information.

--- R/data.R
+++ R/data.R
@@ -1,17 +1,21 @@
 #' Lecixon without Part of Speech
 #'
-#' Lexicon without part of speech is developed by Prof. Chris Khoo of
-#' Wee Kim Wee School of Information and Communication, Nanyang
-#' Technological University.
+#' (c) 2017, Wee Kim Wee School of Communication & Information,
+#' Nanyang Technological University, Singapore
+#' This WKWSCI Sentiment Lexicon ver 1.1 is licensed under the
+#' Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+#' International License (http://creativecommons.org/licenses/by-nc-sa/4.0/)
 #'
 #' @format A data frame with two variables: \code{term} and \code{sentiment}.
 'without_pos'
 
 #' Lecixon with Part of Speech
 #'
-#' Lexicon with part of speech is developed by Prof. Chris Khoo of
-#' Wee Kim Wee School of Information and Communication, Nanyang
-#' Technological University.
+#' (c) 2017, Wee Kim Wee School of Communication & Information,
+#' Nanyang Technological University, Singapore
+#' This WKWSCI Sentiment Lexicon ver 1.1 is licensed under the
+#' Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+#' International License (http://creativecommons.org/licenses/by-nc-sa/4.0/)
 #'
 #' @format A data frame with three variables: \code{term}, \code{POS}
 #' and \code{sentiment}.
@@ -19,20 +23,22 @@
 
 #' Multiword terms
 #'
-#' Lexicon that contains multiword terms, a.k.a short phrases.
-#' It is developed by Prof. Chris Khoo of
-#' Wee Kim Wee School of Information and Communication, Nanyang
-#' Technological University.
+#' (c) 2017, Wee Kim Wee School of Communication & Information,
+#' Nanyang Technological University, Singapore
+#' This WKWSCI Sentiment Lexicon ver 1.1 is licensed under the
+#' Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+#' International License (http://creativecommons.org/licenses/by-nc-sa/4.0/)
 #'
 #' @format A data frame with two variables: \code{term} and \code{sentiment}.
 'multiword_terms'
 
 #' Modifier lecixon
 #'
-#' Lexicon contains modifiers such as absolutely and barely.
-#' It is developed by Prof. Chris Khoo of
-#' Wee Kim Wee School of Information and Communication, Nanyang
-#' Technological University.
+#' (c) 2017, Wee Kim Wee School of Communication & Information,
+#' Nanyang Technological University, Singapore
+#' This WKWSCI Sentiment Lexicon ver 1.1 is licensed under the
+#' Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+#' International License (http://creativecommons.org/licenses/by-nc-sa/4.0/)
 #'
 #' @format A data frame with three variables: \code{modifier}, \code{type}
 #' and \code{POS}.
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# wkwscilexicon
-A lexicon that is developed by Prof Khoo Soo Guan, Christopher and his team at Wee Kim Wee School of Communication, Nanyang Technological University
+# wkwscilex
+WKWSCI Sentiment Lexicon v1.1 is a general-purpose sentiment lexicon,
+    based on the 12dicts common American English word lists compiled by Alan Beale.
+    The lexicon was completed at the end of 2016. The lexicon contains 29,729 words
+    tagged with 4 parts-of-speech: adjective, adverb, noun, and verb. The lexicon
+    comprises 3,187 positive words, 7,247 negative words and 19,295 neutral words.
+    WKWSCI Sentiment Lexicon v1.0 was described and compared with five existing lexicons
+    in Khoo & Johnkhan (2018). Version 1.1 includes some improvements resulting from the
+    reported study.
+Cite: Khoo, C.S.G., & Johnkhan, S.B. (2018). Lexicon-based sentiment analysis: Comparative evaluation of six sentiment lexicons. Journal of Information Science, 44(4), 491-511. https://doi.org/10.1177/0165551517703514
